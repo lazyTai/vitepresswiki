8b1eab902ed4f3d528fd7b47f93403abea6bdefc
lazyTai <1352983234@qq.com>
2024-05-10 18:50:16 +0800

    2024-05-10 18:50:15

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,5 +1,12 @@
 module.exports = {
+      themeConfig: {
+        search: {
+          provider: 'local'
+        },
+       
+      },
     base:'/vitepresswiki/',
     title: '刘明泰的博客',
-    description: '刘明泰的博客'
+    description: '个人联系方式 188194448261(同微信)'，
+    
   }
